d1ee71942c57e480958500749c09786da362d28a
pydevnepal <rajukhatri054@gmail.com>
2021-11-14 15:20:48 +0545

    implement match conditionals

--- src/bin/a4a.rs
+++ src/bin/a4a.rs
@@ -1,4 +1,6 @@
 // Topic: Decision making with match
+// match is similar to if..else condition
+// Every possible option must be accounted for in match
 //
 // Program requirements:
 // * Display "it's true" or "it's false" based on the value of a variable
@@ -7,4 +9,15 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    // initialize the variable
+    // let my_bool = true;
+    let my_bool = false;
+
+    // start match the conditional statement 
+    match my_bool{
+        true => println!("it's true"),
+        false => println!("it's false"),
+    }
+
+}
