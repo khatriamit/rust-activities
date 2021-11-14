5a00156762b654321215edae59ee3c3dc6537856
pydevnepal <rajukhatri054@gmail.com>
2021-11-14 09:01:57 +0545

    start rust learn project

---
+++ README.md
@@ -0,0 +1,4 @@
+<p> compile and run rust code without logs </p>
+<ul>
+    <li>cargo run -q --bin {file_name}</li>
+</ul>
---
+++ src/bin/a1.rs
@@ -0,0 +1,12 @@
+
+fn first_name(){
+    println!("amit")
+}
+
+fn last_name(){
+    println!("khatri")
+}
+fn main() {
+    first_name();
+    last_name();
+}
