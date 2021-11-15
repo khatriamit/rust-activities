3dc40f281db3fb16e85f55e47719439252c60703
pydevnepal <rajukhatri054@gmail.com>
2021-11-15 19:52:51 +0545

    implement emum

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,25 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color{
+    Blue,
+    Red,
+    Black,
+    Yellow,
+    Orange
+}
+
+fn check_color(color: Color){
+    match color{
+        Color::Blue => println!("blue"),
+        Color::Red => println!("red"),
+        Color::Black => println!("black"),
+        Color::Yellow => println!("yellow"),
+        Color::Orange => println!("orange"),
+    }
+       
+}
+
+fn main() {
+    check_color(Color::Black);
+}
