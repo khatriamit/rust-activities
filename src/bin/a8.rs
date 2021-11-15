f4152339b7989bbb2cdf18dae1cc8d3340a58b75
pydevnepal <rajukhatri054@gmail.com>
2021-11-15 19:53:09 +0545

    implement emum with struct

--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -1,4 +1,4 @@
-// Topic: Organizing similar data using structs
+// Topic: Organizing similar data using struct
 //
 // Requirements:
 // * Print the flavor of a drink and it's fluid ounces
@@ -9,4 +9,49 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+
+// flavour enum initialize
+enum Flavour{
+    Cola,
+    Sprite,
+    Fruity,
+}
+
+// drink struct initialize with flavour and fluid ounce data types
+struct Drink{
+    flavour: Flavour,
+    fluid_oz: f64,
+}
+
+// function to print out drink flavor and ounces
+fn print_drink(drink: Drink){
+    
+    //match expression to print the drink flavor
+    match drink.flavour {
+        Flavour::Cola => println!("flavour: Cola "),
+        Flavour::Sprite => println!("flavour: Sprite"),
+        Flavour::Fruity => println!("flavour: Fruity"),
+    }
+
+    println!("oz: {:?}", drink.fluid_oz);
+}
+
+fn main() {
+    let cola = Drink{
+        flavour: Flavour::Cola,
+        fluid_oz: 3.5,
+    };
+
+    let sprite = Drink{
+        flavour: Flavour::Sprite,
+        fluid_oz: 5.5,
+    };
+
+    let fruity = Drink{
+        flavour: Flavour::Fruity,
+        fluid_oz: 2.5,
+    };
+    print_drink(cola);
+    print_drink(sprite);
+    print_drink(fruity);
+}
