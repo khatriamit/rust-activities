c7797df35e84ed6baedcbb823caee2169386fbdf
pydevnepal <rajukhatri054@gmail.com>
2021-11-23 21:34:04 +0545

    implement impl logic

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,63 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+// * Use a struct to encapsulate the box characteristics
+enum Color {
+    Brown,
+    Red,
+}
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Brown => println!("brown"),
+            Color::Red => println!("red"),
+        }
+    }
+}
+
+struct Dimensions {
+    height: f64,
+    width: f64,
+    depth: f64,
+}
+
+impl Dimensions {
+    fn print(&self) {
+        println!("width: {:?}", self.width);
+        println!("height: {:?}", self.height);
+        println!("depth: {:?}", self.depth);
+    }
+}
+
+// * Use a struct to encapsulate the box characteristics
+struct ShippingBox {
+    color: Color,
+    dimensions: Dimensions,
+    weight: f64,
+}
+
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight: {:?}", self.weight);
+    }
+}
+
+fn main() {
+    let small_dimensions = Dimensions {
+        width: 100.0,
+        height: 150.5,
+        depth: 10.2,
+    };
+    let small_box = ShippingBox::new(5.0, Color::Red, small_dimensions);
+    small_box.print();
+}
