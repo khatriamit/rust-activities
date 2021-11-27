e31c171f703d9281c7f53ebe23ae6cf8833afeb1
pydevnepal <rajukhatri054@gmail.com>
2021-11-27 16:22:50 +0545

    advance vector methods

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -11,4 +11,30 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+// * Use an enum for the tickets with data associated with each variant
+enum Tickets {
+    Backstage(f64, String),
+    Standard(f64),
+    Vip(f64, String),
+}
+fn main() {
+    // * Create one of each ticket and place into a vector
+    let tickets = vec![
+        Tickets::Backstage(50.0, "John".to_owned()),
+        Tickets::Standard(15.0),
+        Tickets::Vip(30.0, "Mike".to_owned()),
+    ];
+
+    // * Use a match expression while iterating the vector to print the ticket info
+    for ticket in tickets {
+        match ticket {
+            Tickets::Backstage(price, holder) => {
+                println!("Backstage Ticket Holder: {:?}, price: {:?}", holder, price)
+            }
+            Tickets::Standard(price) => println!("Standard ticket price: {:?}", price),
+            Tickets::Vip(price, holder) => {
+                println!("Vip Ticket Holder: {:?}, price: {:?}", holder, price)
+            }
+        }
+    }
+}
