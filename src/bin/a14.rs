e324b3d191ab4288565ae807f7c8e8a5c8c506be
pydevnepal <rajukhatri054@gmail.com>
2021-11-27 10:05:13 +0545

    implement vector with string

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,50 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+// * Use a struct for a persons age, name, and favorite color
+// * The color and name should be stored as a String
+struct Person {
+    age: i32,
+    name: String,
+    fav_color: String,
+}
+
+// * The name and colors should be printed using a function
+fn print_name(name: &str) {
+    println!("Name: {:?}", name);
+}
+
+fn print_color(color: &str) {
+    println!("Color: {:?}", color);
+}
+
+fn main() {
+    // * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            age: 10,
+            name: "Brother".to_owned(),
+            fav_color: "Yellow".to_owned(),
+        },
+        Person {
+            age: 14,
+            name: String::from("Big Brother"),
+            fav_color: String::from("Black"),
+        },
+        Person {
+            age: 6,
+            name: String::from("Small Sister"),
+            fav_color: "Pink".to_owned(),
+        },
+    ];
+
+    // * Iterate through the vector using a for..in loop
+    for person in people {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age <= 10 {
+            println!("age: {:?}", person.age);
+            print_name(&person.name);
+            print_color(&person.fav_color);
+        }
+    }
+}
